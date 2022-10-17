fcecd55727a876988bb1ab17f2eba0974a429ff5
Zpalfi42 <zsolt.palfi02@gmail.com>
2022-10-17 14:13:07 +0200

    Data saved in a .csv file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from multiprocessing import Barrier
-from urllib.request import urlopen
+import collections
+from multiprocessing.spawn import import_main_path
+from urllib.request import urlopen, Request
 import  json
+from PyPDF2 import PdfFileReader
+import io
+import  pandas as pd
 
 basicurl = 'https://opendata-ajuntament.barcelona.cat/data'
 url = 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=ed515bb8-502b-4dff-96dc-769f72767919&limit=5&q=title:jones'
@@ -18,9 +22,29 @@ parse_start = json.loads(startobj.read())
 
 parser = parse_start['result']['records']
 
+df = pd.DataFrame([], columns=['nom_barri', 'poblacio_sola', 'titulats_superiors', 'nombre_aturats' , 'renda', 'sobreenvelliment'])
+
 for x in range(73):
     barri = parser[x]
-    barri_name = barri['NOM_DIVISIO_TERRITORIAL']
+    NB = barri['NOM_DIVISIO_TERRITORIAL']
     barri_pdf = barri['URL_FITXA_DIVISIO_TERRITORIAL']
-    print(barri_name)
-    print(barri_pdf)
+    #print(barri_name)
+    #print(barri_pdf)
+    pdf = urlopen(Request(barri_pdf)).read()
+    memory_file = io.BytesIO(pdf)
+    pdf_file = PdfFileReader(memory_file)
+    page = pdf_file.getPage(1)
+    page_content = page.extract_text()
+    i = page_content.find('Població de més de 65 anys que viu sola (%)  (2)')
+    PS = page_content[(i + 49):page_content.find(' ', i + 49)]
+    i = page_content.find('% Titulats superiors i CFGS (1)')
+    TS = page_content[(i + 32):page_content.find(' ', i + 32)]
+    i = page_content.find("Nombre d'aturats registrats (5)")
+    NA = page_content[(i + 32):page_content.find(' ', i + 32)]
+    i = page_content.find("Renda familiar disponible per habitant (2017)")
+    RF = page_content[(i + 46):page_content.find(' ', i + 46)]
+    i = page_content.find("Índex de sobreenvelliment   (3)")
+    IS = page_content[(i + 32):page_content.find(' ', i + 32)]
+    df1 = pd.DataFrame([[NB, PS, TS, NA, RF, IS]], columns=['nom_barri', 'poblacio_sola', 'titulats_superiors', 'nombre_aturats' , 'renda', 'sobreenvelliment'])
+    df = pd.concat([df, df1])
+df.to_csv('data.csv', index=False)
